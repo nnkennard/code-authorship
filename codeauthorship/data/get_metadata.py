65b998b349d25638967a2a6c50256e810f724f86
Andrew Drozdov <andrew@mrdrozdov.com>
2019-03-04 10:37:20 -0500

    Add command line args.

--- codeauthorship/data/get_metadata.py
+++ codeauthorship/data/get_metadata.py
@@ -1,10 +1,24 @@
+import argparse
+import os
 import codeauthorship.codeauth_lib as codeauth_lib
 
-def main():
+def main(options):
 
+  # Assign files according to command line args.
+  filenames = []
+  if options.yearstart <= 0:
+    filenames.append(options.data_path)
+  else:
+    if options.yearend <= 0:
+      filenames.append(os.path.join(options.data_path, 'gcj{}.csv'.format(options.yearstart)))
+    else:
+      for year in range(options.yearstart, options.yearend):
+        filenames.append(os.path.join(options.data_path, 'gcj{}.csv'.format(year)))
+
+  # Read files.
   rows = []
-  for year in range(2008, 2017):
-    reader = codeauth_lib.file_getter(year)
+  for filename in filenames:
+    reader = codeauth_lib.file_getter(filename)
     for row in reader:
       num_chars = str(len(str(row["flines"])))
       row_list = (row["year"], row["round"], row["username"], row["solution"],
@@ -15,4 +29,10 @@ def main():
     print("\t".join(row))
 
 if __name__ == "__main__":
-  main()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--data_path', default='/iesl/canvas/nnayak/data/codeauth_data/gcj-dataset', type=str)
+  parser.add_argument('--yearstart', default=2008, type=int)
+  parser.add_argument('--yearend', default=2017, type=int)
+  options = parser.parse_args()
+
+  main(options)
--- codeauthorship/data/get_python_files.py
+++ codeauthorship/data/get_python_files.py
@@ -1,13 +1,28 @@
 from io import BytesIO
 import io
+import os
 import tokenize
 import codeauthorship.codeauth_lib as codeauth_lib
+import argparse
 
-def main():
 
+def main(options):
+
+  # Assign files according to command line args.
+  filenames = []
+  if options.yearstart <= 0:
+    filenames.append(options.data_path)
+  else:
+    if options.yearend <= 0:
+      filenames.append(os.path.join(options.data_path, 'gcj{}.csv'.format(options.yearstart)))
+    else:
+      for year in range(options.yearstart, options.yearend):
+        filenames.append(os.path.join(options.data_path, 'gcj{}.csv'.format(year)))
+
+  # Read files.
   rows = []
-  for year in range(2008, 2017):
-    reader = codeauth_lib.file_getter(year)
+  for filename in filenames:
+    reader = codeauth_lib.file_getter(filename)
     for i, row in enumerate(reader):
       num_chars = str(len(str(row["flines"])))
       file_type = row["full_path"].split(".")[-1]
@@ -25,4 +40,10 @@ def main():
 
 
 if __name__ == "__main__":
-  main()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--data_path', default='/iesl/canvas/nnayak/data/codeauth_data/gcj-dataset', type=str)
+  parser.add_argument('--yearstart', default=2008, type=int)
+  parser.add_argument('--yearend', default=2017, type=int)
+  options = parser.parse_args()
+
+  main(options)
