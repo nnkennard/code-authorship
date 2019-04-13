a18c1271516401ac20f1c6175ac0aabb62bf3d1c
Andrew Drozdov <andrew@mrdrozdov.com>
2019-04-13 10:53:48 -0400

    Use Neha's CV.

--- codeauthorship/scripts/train-baseline.py
+++ codeauthorship/scripts/train-baseline.py
@@ -10,20 +10,7 @@ import numpy as np
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-
-# def get_dataset_from_file(file_name):
-#     files, labels = [], []
-#     dataset = collections.defaultdict(list)
-#     with open(file_name, 'r') as f:
-#         for i, line in enumerate(f):
-#             username, tokens = line.strip().split("\t", 1)
-#             files.append(tokens), labels.append(username)
-#             if i == 300:
-#                 break
-#     label_vocab = {label:i for i, label in enumerate(sorted(set(labels)))}
-#     converted_labels = [label_vocab[label] for label in labels]
-#     return files, np.array(converted_labels), label_vocab
+from sklearn.model_selection import StratifiedKFold
 
 
 def indexify(value2idx, lst):
@@ -90,48 +77,24 @@ def get_dataset(path):
     return dataset
 
 
-def run(options):
-    random.seed(options.seed)
-    dataset = get_dataset(options.path_in)
+def run_train(X, Y):
+    model = RandomForestClassifier(n_estimators=100, max_depth=None, n_jobs=-1, random_state=0)
+    model.fit(X, Y)
+    results = {}
+    results['model'] = model
+    return results
 
-    print('dataset-size = {}'.format(dataset['metadata']['dataset_size']))
-    print('vocab-size = {}'.format(dataset['metadata']['vocab_size']))
-    print('# of classes = {}'.format(dataset['metadata']['n_classes']))
 
-    label2idx = dataset['metadata']['label2idx']
-    idx2label = {v: k for k, v in label2idx.items()}
-    labels = dataset['secondary']['labels']
+def run_evaluation(model, X, Y):
+    predictions = model.predict(X)
+    acc = np.mean(predictions == Y)
+    results = {}
+    results['acc'] = acc
+    return results
 
-    contents = [' '.join(x) for x in dataset['primary']]
 
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(contents)
-
-    # Create data split.
-
-    ## Shuffle.
-    index = list(range(len(labels)))
-    random.shuffle(index)
-    author2idx = {}
-    for idx in index:
-        label = labels[idx]
-        author2idx.setdefault(label, []).append(idx)
-
-    ## Create split.
-    cutoff = 4
-    train_idx = []
-    test_idx = []
-    for k in author2idx.keys():
-        if len(author2idx[k]) <= cutoff * 2:
-            continue
-        test_idx += author2idx[k][:cutoff]
-        train_idx += author2idx[k][cutoff:cutoff*2]
-
-    # Create data.
-    trainX = X[train_idx]
-    trainY = [labels[idx] for idx in train_idx]
-    testX = X[test_idx]
-    testY = [labels[idx] for idx in test_idx]
+def run_experiment(trainX, trainY, testX, testY):
+    label2freq = Counter(trainY)
 
     # Train and Predict
     clf = RandomForestClassifier(n_estimators=100, max_depth=None, n_jobs=-1, random_state=0)
@@ -139,7 +102,6 @@ def run(options):
     predictions = clf.predict(testX)
 
     # Get F1 by frequency (Note: This doesn't help anymore since everything is same freq).
-    label2freq = Counter(trainY)
     freq2metrics = {}
     for yhat, y in zip(predictions, testY):
         true_freq = label2freq[y]
@@ -165,8 +127,8 @@ def run(options):
         precision = true_pos / (true_pos + false_pos) if (true_pos + false_pos) > 0 else 0
         recall = true_pos / (true_pos + false_neg) if  (true_pos + false_neg) else 0
         f1 = 2 * precision * recall / (precision + recall) if (precision + recall) else 0
-        print('freq={} precision={:.3f}, recall={:.3f}, f1={:.3f}'.format(
-            k, precision, recall, f1))
+        # print('freq={} precision={:.3f}, recall={:.3f}, f1={:.3f}'.format(
+        #     k, precision, recall, f1))
 
         f1_lst.append(f1)
 
@@ -176,10 +138,81 @@ def run(options):
 
     print('average-f1={:.3f} accuracy={:.3f}'.format(average_f1, accuracy))
 
+
+def run(options):
+    random.seed(options.seed)
+    np.random.seed(options.seed)
+    dataset = get_dataset(options.path_in)
+
+    print('dataset-size = {}'.format(dataset['metadata']['dataset_size']))
+    print('vocab-size = {}'.format(dataset['metadata']['vocab_size']))
+    print('# of classes = {}'.format(dataset['metadata']['n_classes']))
+
+    label2idx = dataset['metadata']['label2idx']
+    idx2label = {v: k for k, v in label2idx.items()}
+    labels = dataset['secondary']['labels']
+
+    contents = [' '.join(x) for x in dataset['primary']]
+
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(contents)
+    Y = np.array(labels)
+
+    # Shuffle.
+    index = np.arange(Y.shape[0])
+    random.shuffle(index)
+    X = X[index]
+    Y = Y[index]
+
+    # Filter to classes with at least 9 instances (and balance labels).
+
+    ## First record 9 instances from each class (ignore classes with less than 9 instances).
+    index = np.arange(Y.shape[0])
+    index_to_keep = []
+    label_set = set(labels)
+    for label in label_set:
+        mask = Y == label
+        if mask.sum() < options.cutoff:
+            continue
+        # TODO: Should we take all of the instances?
+        index_to_keep += index[mask].tolist()[:options.cutoff]
+    index_to_keep = np.array(index_to_keep)
+
+    ## Then filter accordingly.
+    X = X[index_to_keep]
+    Y = Y[index_to_keep]
+
+    # Run k-fold cross validation.
+
+    acc_lst = []
+
+    cross_validation_splitter = StratifiedKFold(n_splits=9)
+
+    for i, (train_index, test_index) in enumerate(cross_validation_splitter.split(X, Y)):
+        trainX, testX = X[train_index], X[test_index]
+        trainY, testY = Y[train_index], Y[test_index]
+        train_results = run_train(trainX, trainY)
+        model = train_results['model']
+        eval_results = run_evaluation(model, testX, testY)
+        acc = eval_results['acc']
+
+        train_size = trainX.shape[0]
+        test_size = testX.shape[0]
+
+        print('fold={} train-size={} test-size={} acc={:.3f} '.format(
+            i, train_size, test_size, acc))
+
+        acc_lst.append(acc)
+
+    average_acc = np.mean(acc_lst)
+
+    print('average-acc={:.3f}'.format(average_acc))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--path_in', default='~/Downloads/gcj2008.csv.jsonl', type=str)
     parser.add_argument('--seed', default=None, type=int)
+    parser.add_argument('--cutoff', default=9, type=int)
     options = parser.parse_args()
 
     options.path_in = os.path.expanduser(options.path_in)
